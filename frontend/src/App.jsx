2e1c34285685baea182b8bde0a89d0ab609f8171
mnvrohith <mnvrohith@gmail.com>
2025-03-19 23:06:12 +0530

    Add initial project setup with React, Axios, Goober dependencies and signup,login,logout pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,48 @@
+import Navbar from './components/Navbar';
+import{ Routes, Route } from "react-router-dom";
+import HomePage from './pagess/HomePage';
+import SignUpPage from './pagess/SignUpPage';
+import LoginPage from './pagess/LoginPage';
+import SettingsPage from './pagess/SettingsPage';
+import ProfilePage from './pagess/ProfilePage';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import { Loader } from "lucide-react";
+import {Toaster} from "react-hot-toast";
+
 const App = () => {
+  const {authUser,checkAuth,isCheckingAuth,onlineUsers}= useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }
+  ,[checkAuth]);
+console.log({authUser});
+
+if(isCheckingAuth && !authUser)return(
+  <div className="felx items-center justify-center h-screen">
+   <Loader className = "size-10 animate-spin"/>
+  </div>
+);
+
+
+
   return(
-    <div className = "text-red-500">Hello</div>
+    <div >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/"/>} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
+      </Routes>
+
+       <Toaster/>
+
+
+
+    </div>
   )
 };
 
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,68 @@
+import {create} from "zustand";
+import {axiosInstance} from "../lib/axios.js"
+export const useAuthStore = create((set) => ({
+ authUser: null,
+ isSigningUp: false,
+ isLoggingIng: false,
+ isUpdatingProfile: false,
+ 
+ isCheckingAuth: true,
+
+ checkAuth:async() =>{
+    try{
+        const res = await axiosInstance.get("/auth/check");
+
+        set({authUser:res.data});
+    }catch (error){
+        console.log("error in checkAuth:",error);
+set({authUser:res.data});
+    }finally{
+        set({isCheckingAuth:false});
+    }
+ },
+
+signup: async(data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+
+    
+},
+
+logout:async()=>{
+    try {
+        await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+        get().disconnectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+
+},
+
+login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+
+}));
