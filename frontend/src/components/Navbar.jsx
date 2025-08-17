11ea73772deb11ab57846541b9f7d876e930f7da
mnvrohith <mnvrohith@gmail.com>
2025-08-17 14:00:41 +0530

    changed settings page layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, MessagesSquare, Settings, User } from "lucide-react";
 
 
 const Navbar=()=> {
@@ -17,9 +17,9 @@ const Navbar=()=> {
         <div className="flex items-center gap-8">
           <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
             <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-              <MessageSquare className="w-5 h-5 text-primary" />
+              <MessagesSquare className="w-5 h-5 text-primary" />
             </div>
-            <h1 className="text-lg font-bold">Chatty</h1>
+            <h1 className="text-lg font-bold">ChatBoard</h1>
           </Link>
         </div>
 
@@ -32,8 +32,11 @@ const Navbar=()=> {
             `}
           >
             <Settings className="w-4 h-4" />
+           
+
             <span className="hidden sm:inline">Settings</span>
           </Link>
+          
 
           {authUser && (
             <>
