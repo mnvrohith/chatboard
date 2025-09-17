049ca71e31072178db18b5b5a01ad060144d6662
mnvrohith <mnvrohith@gmail.com>
2025-09-17 12:35:36 +0530

    refactor: remove unused import from useAuthStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,6 @@
 import {create} from "zustand";
 import {axiosInstance} from "../lib/axios.js";
 import { toast } from "react-hot-toast";
-// import { disconnect } from "mongoose";
 import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5173";
